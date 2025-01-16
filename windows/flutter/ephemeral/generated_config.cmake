7fc3d6762f46c82d13c6cbd221071cfcfecfd47f
SuryaTeja122 <suryabhiguva@gmail.com>
2025-01-16 14:57:52 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,23 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\ECom\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\91889\\OneDrive\\Desktop\\food\\food_deliver\\e_comm_food" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\ECom\\flutter"
+  "PROJECT_DIR=C:\\Users\\91889\\OneDrive\\Desktop\\food\\food_deliver\\e_comm_food"
+  "FLUTTER_ROOT=D:\\ECom\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\91889\\OneDrive\\Desktop\\food\\food_deliver\\e_comm_food\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\91889\\OneDrive\\Desktop\\food\\food_deliver\\e_comm_food"
+  "FLUTTER_TARGET=C:\\Users\\91889\\OneDrive\\Desktop\\food\\food_deliver\\e_comm_food\\lib\\main.dart"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\91889\\OneDrive\\Desktop\\food\\food_deliver\\e_comm_food\\.dart_tool\\package_config.json"
+)
